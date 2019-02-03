58b3da6898285107ab08f5c87a2585e898fb90e9
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-02-03 00:15:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function getRepositories() {
 
 function showRepositories(event, data) {
     const repos = JSON.parse(this.responseText)
+    const src = document.getElementById("repository-template").innerHTML
     const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join("")}</ul>`
     document.getElementById("repositories").innerHTML = repoList
 }
